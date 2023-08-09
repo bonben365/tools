9c3490ec7664a7044cb2ca44499a8463bba11764
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-08-09 09:46:35 +0700

    Update dns-changer.ps1

--- dns-changer.ps1
+++ dns-changer.ps1
@@ -1,10 +1,10 @@
 Write-Host
-Write-Host "Danh sách card mạng trên máy tính:"
+Write-Host "Danh sách card mạng trên máy tính:" -ForegroundColor Yellow
 Get-DnsClientServerAddress
 Write-Host
-$InterfaceAlias = Read-Host -Prompt 'Nhập vào tên của card mạng mà bạn muốn thay đổi DNS Servers:'
+$InterfaceAlias = Read-Host -Prompt 'Nhập vào tên của card mạng mà bạn muốn thay đổi DNS Servers' -ForegroundColor Yellow
 Set-DNSClientServerAddress "$InterfaceAlias" –ServerAddresses ("8.8.8.8", "1.1.1.1")
 
 Write-Host
-Write-Host "Kiểm tra lại DNS Server đã được thay đổi thành công:"
+Write-Host "Kiểm tra lại DNS Server đã được thay đổi thành công:" -ForegroundColor Green
 Get-DnsClientServerAddress
