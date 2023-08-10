47b56f453f6556b69abcb01b9eca21e28eb5cc57
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-08-10 11:09:51 +0700

    Update choco.ps1

--- choco.ps1
+++ choco.ps1
@@ -1,3 +1,8 @@
+if (-not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Warning "You need to have Administrator rights to run this script!`nPlease re-run this script as an Administrator in an elevated powershell prompt!"
+    break
+}
+
 $path = 'C:\ProgramData\chocolatey'
 if (-not (Test-Path -Path $path)) {
     Set-ExecutionPolicy Bypass -Scope Process -Force
